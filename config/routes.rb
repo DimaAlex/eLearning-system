746d9f63c98589b058b5639a02498663ded4aa66
dima.shutov.developer <dima.shutov.developer@gmail.com>
2016-03-20 01:01:22 +0300

    delete unnecessary controller User and view User

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root to: 'home#index'
 
+
   devise_for :users, controllers: {
-      sessions: 'users/sessions'
+      sessions: 'devise/sessions'
   }
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
