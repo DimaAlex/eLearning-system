a0222946c111bf5b1551f61fd670596cc229cb99
ddzyanis <ddzyanis@mail.ru>
2016-03-23 17:13:20 +0300

    jquery choise recipients

--- app/assets/javascripts/func.js
+++ app/assets/javascripts/func.js
@@ -1,7 +1,31 @@
-jQuery(function(){
-  jQuery("input[type='radio'][name='recipient']").each(function(index, button){
-    jQuery(button).click(function(){
-      alert("dsfdg");
-    });
+$(document).ready(function(){
+  $('input[type=radio][name=recipient]').change(function() {
+    if (this.value == 'email') {
+      alert("EMAIL");
+    }
+    else if (this.value == 'last_name') {
+      alert("LAST_NAME");
+    }
+
+    // $.ajax({
+    //     url: 'mycontr/get_users',
+    //     method: 'GET',
+    //     datatype: 'json'
+    //   }).success(functio(data){
+
+    //     $.each(data, function(value,key) {
+    //       $("#id").append($("<option></option>")
+    //          .attr("value", value).text(key));
+    //     });
+
+    //   })
   });
-});
+
+  // $("#2").change(function(){
+  //   alert("2234");
+  });
+
+
+
+//render :json {data[email] }
+
