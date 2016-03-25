4894c10d300a91fe4cdeeab4c4248b7caccdbcce
ddzyanis <ddzyanis@mail.ru>
2016-03-25 13:33:17 +0300

    delete mailbox emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,6 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   # config.secret_key_base = ENV[“SECRET_KEY_BASE”]
 
-  config.action_mailer.default_url_options = { host: 'www.secret-lake-41464.herokuapp.com'}
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
