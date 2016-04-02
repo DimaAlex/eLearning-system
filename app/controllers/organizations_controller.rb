f793e1c3df78772c570193a9f78dc4bf662e00ac
ddzyanis <ddzyanis@mail.ru>
2016-04-02 23:43:13 +0300

    add bootstrap pagination

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -59,7 +59,7 @@ class OrganizationsController < ApplicationController
 
   def users_in_org
     @users_not_in_org = User.all - @organization.users
-    @users_in_org = @organization.users.usual_users_in_org.paginate(:page => params[:page], :per_page => 20)
+    @users_in_org = @organization.users.usual_users_in_org.paginate(page: params[:page], per_page: 20)
   end
 
   def create_users_to_org
