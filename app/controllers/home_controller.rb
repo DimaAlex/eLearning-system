faae837a8c31efc2ea4d7be28c499f5f249e40d5
KaterinaSav <caterinasav@yandex.ru>
2016-04-05 17:03:29 +0300

    show current courses on main page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      @user = current_user
+      @current_courses = @user.users_courses.where(is_started: true).map {|x| x.course}
+    end
   end
 end
