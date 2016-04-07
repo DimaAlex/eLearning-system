52c0faff6de1b227ee6d0598acb84c43db8a6dc7
ddzyanis <ddzyanis@mail.ru>
2016-04-08 01:33:10 +0300

    get public courses only

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     if user_signed_in?
       @user = current_user
-      @courses = Course.last(6)
+      @public_courses = Course.where(permission: 'Public')
       @current_courses = @user.users_courses.where(is_started: true).map {|x| x.course}
       @finished_courses = @user.users_courses.where(is_finished: true).map {|x| x.course}
     end
