ee788dcdee7cb53b908d5805b8b9f38bd53f266a
DimaAlex <dimaalekseichik@gmail.com>
2016-04-16 14:28:59 +0300

    change db/seeds.rb, made more useful

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,68 +1,49 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# 15.times do |i|
-#   user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "11111111", password_confirmation: "11111111")
-# end
-# User.all.each do |user|
-#   user.country = Faker::Address.country
-#   user.save
-# end
+15.times do
+  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "11111111", password_confirmation: "11111111")
+end
 
-# 20.times do |i|
-#   organization = Organization.create(title: Faker::Company.name, phone: Faker::PhoneNumber.cell_phone, description: Faker::Lorem.paragraph(2))
-#   organization.save
-# end
+User.all.each do |user|
+  user.country = Faker::Address.country
+  user.save
+end
 
-# 20.times do |i|
-#   imper = AdminsImpersonation.create(user_id: 1, admin_id: 31)
-#   imper.save
-# end
+20.times do
+  organization = Organization.create(title: Faker::Company.name, phone: Faker::PhoneNumber.cell_phone, description: Faker::Lorem.paragraph(2))
+  organization.save
+end
 
+20.times do
+  imper = AdminsImpersonation.create(user_id: 1, admin_id: 31)
+  imper.save
+end
 
-# 30.times do |i|
-#   course = Course.create(title: Faker::Company.name,
-#                          permission: "Public",
-#                          author_id: rand(121..130),
-#                          author_type: "Organization"
-#                          )
-#   page = Page.new(title: Faker::Company.name,
-#                      course_id: course.id,
-#                      page_type: "Lecture",
-#                      body: "Example")
-#   page.save
-#   course.save
-# end
+30.times do
+  course = Course.create(title: Faker::Company.name,
+                         permission: "Public",
+                         author_id: rand(Organization.first.id..Organization.last.id),
+                         author_type: "Organization"
+                         )
+  page = Page.new(title: Faker::Company.name,
+                     course_id: course.id,
+                     page_type: "Lecture",
+                     body: "Example")
+  page.save
+  course.save
+end
 
-# 10.times do |i|
-#   course = Course.create(title: Faker::Company.name,
-#                          permission: "Public",
-#                          author_id: 80,
-#                          author_type: "User"
-#                          )
-#   page = Page.new(title: Faker::Company.name,
-#                      course_id: course.id,
-#                      page_type: "Lecture",
-#                      body: "Example")
-#   page.save
-#   course.save
-# end
-
-# 10.times do |i|
-#   course = Course.create(title: Faker::Company.name,
-#                          permission: "Public",
-#                          author_id: 49,
-#                          author_type: "User"
-#                          )
-#   page = Page.new(title: Faker::Company.name,
-#                      course_id: course.id,
-#                      page_type: "Lecture",
-#                      body: "Example")
-#   page.save
-#   course.save
-# end
+20.times do
+  course = Course.create(title: Faker::Company.name,
+                         permission: "Public",
+                         author_id: rand(User.first.id..User.last.id),
+                         author_type: "User"
+                         )
+  page = Page.new(title: Faker::Company.name,
+                     course_id: course.id,
+                     page_type: "Lecture",
+                     body: "Example")
+  page.save
+  course.save
+end
