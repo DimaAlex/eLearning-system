03784e4cb3fc687c545abc1ee51d2a807698cbd1
DimaAlex <dimaalekseichik@gmail.com>
2016-04-17 02:47:58 +0300

    add user's organizations in profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   end
 
   def organizations
-    @organizations = @user.organizations
+    @organizations = @user.organizations.paginate(page: params[:page], per_page: 4)
   end
 
   def show
