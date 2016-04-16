a77f51f885fb3a21f73a34459113a52e52281238
ddzyanis <ddzyanis@mail.ru>
2016-04-16 21:47:57 +0300

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,21 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# 15.times do |i|
-#   user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "11111111", password_confirmation: "11111111")
-# end
-# User.all.each do |user|
-#   user.country = Faker::Address.country
-#   user.save
-# end
+15.times do
+  user = User.create(first_name: Faker::Name.first_name,
+                     last_name: Faker::Name.last_name,
+                     email: Faker::Internet.email,
+                     country:  Faker::Address.country,
+                     password: "11111111",
+                     password_confirmation: "11111111"
+                     )
+  user.save
+end
 
-# 20.times do |i|
-#   organization = Organization.create(title: Faker::Company.name, phone: Faker::PhoneNumber.cell_phone, description: Faker::Lorem.paragraph(2))
-#   organization.save
-# end
+20.times do
+  organization = Organization.create(title: Faker::Company.name, phone: Faker::PhoneNumber.cell_phone, description: Faker::Lorem.paragraph(2))
+  organization.save
+end
 
 # 20.times do |i|
 #   imper = AdminsImpersonation.create(user_id: 1, admin_id: 31)
@@ -25,33 +28,37 @@
 # end
 
 
-# 30.times do |i|
-#   course = Course.create(title: Faker::Company.name,
-#                          permission: "Public",
-#                          author_id: rand(121..130),
-#                          author_type: "Organization"
-#                          )
-#   page = Page.new(title: Faker::Company.name,
-#                      course_id: course.id,
-#                      page_type: "Lecture",
-#                      body: "Example")
-#   page.save
-#   course.save
-# end
+80.times do
+  course = Course.create(title: Faker::Company.name,
+                         permission: "Public",
+                         author_id: rand(1..20),
+                         author_type: "Organization"
+                         )
+  3.times do
+    page = Page.new(title: Faker::Company.name,
+                    course_id: course.id,
+                    page_type: "Lecture",
+                    body: "Example")
+    page.save
+  end
+  course.save
+end
 
-# 10.times do |i|
-#   course = Course.create(title: Faker::Company.name,
-#                          permission: "Public",
-#                          author_id: 80,
-#                          author_type: "User"
-#                          )
-#   page = Page.new(title: Faker::Company.name,
-#                      course_id: course.id,
-#                      page_type: "Lecture",
-#                      body: "Example")
-#   page.save
-#   course.save
-# end
+60.times do |i|
+  course = Course.create(title: Faker::Company.name,
+                         permission: "Public",
+                         author_id: rand(1..10),
+                         author_type: "User"
+                         )
+  3.times do
+    page = Page.new(title: Faker::Company.name,
+                    course_id: course.id,
+                    page_type: "Lecture",
+                    body: "Example")
+    page.save
+  end
+  course.save
+end
 
 # 10.times do |i|
 #   course = Course.create(title: Faker::Company.name,
