2c64751df72048f2f1ddf2b7abb507faaf44340f
DimaAlex <dimaalekseichik@gmail.com>
2016-04-17 18:27:07 +0300

    add tests on Organization#is_org_admin?

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,12 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-  before { @organization = Organization.new(title: "Example",
-                                            description: "Example",
-                                            phone: "Example",
-                                            image_file_name: "Example"
-                                            )
-  }
+  before { @organization = create(:organization) }
 
   subject { @organization }
 
@@ -14,4 +9,23 @@ RSpec.describe Organization, type: :model do
   it { should respond_to(:description) }
   it { should respond_to(:phone) }
   it { should respond_to(:image_file_name) }
+
+  describe '#is_org_admin?' do
+    let(:new_user) { create(:user) }
+    it "return 'false' when user is not admin of organization" do
+      expect(@organization.is_org_admin?(new_user)).to eq false
+    end
+
+    it "return 'false' when user is nil" do
+      expect(@organization.is_org_admin?(nil)).to eq false
+    end
+
+    context 'add user in org admins of organization' do
+      before { @organization.users_organizations.create(user_id: new_user.id, is_org_admin: true) }
+      it "return 'true' if user is org admin" do
+        expect(@organization.is_org_admin?(new_user)).to eq true
+      end
+    end
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'simplecov'
+require 'factory_girl_rails'
 SimpleCov.start
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -8,5 +10,7 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
+
+  config.include FactoryGirl::Syntax::Methods
 
 end
